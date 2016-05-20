ffaf633fd9646eeff795950b86b3252fd7b18941
wh_orangester <w.h.orangester@gmail.com>
2016-05-20 11:03:14 +0700

    add support component: ticket
    - add component/Ticket.php for ticket component
    - modify store.html to display ticket list
    - modify basic.css to support modified store.html
    - add flight.js for ajax associate with displaying tickets

---
+++ component/Ticket.php
@@ -0,0 +1,39 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+
+    //Component Implementation
+    class Ticket{
+        public static function getTicketInfo($flightid) {
+            $conn = new mysqli("127.0.0.1", "root", "", "bloodymary");
+            if($conn->connect_errno) {
+                $conn->close();
+                return -1;
+            }
+            $flightid = $conn->real_escape_string($flightid);
+            $result = $conn->query(
+                "SELECT *
+                FROM ticket
+                WHERE FlightID = '".$flightid."';"
+            );
+            $conn->close();
+            $flightinfo = $result->fetch_array(MYSQL_ASSOC);
+            return $flightinfo;
+        }
+        public static function filterTicket($criteria = null) {
+            $conn = new mysqli("127.0.0.1", "root", "", "bloodymary");
+            if($conn->connect_errno) {
+                $conn->close();
+                return -1;
+            }
+            $flightlist = $conn->query("SELECT * FROM ticket;");
+            $conn->close();
+            $result = array();
+            while($flight = $flightlist->fetch_array(MYSQLI_ASSOC)) $result[] = $flight;
+            return $result;
+        }
+    }
+    //Server-Client Interface
+    if(isset($_POST['flightid'])) echo json_encode(Ticket::getTicketInfo($_POST['flightid']));
+    else echo json_encode(Ticket::filterTicket());
+?>
---
+++ javascript/flight.js
@@ -0,0 +1,37 @@
+function load_cards() {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
+            set_cards(xmlhttp.responseText);
+    }
+    xmlhttp.open("POST", "component/Ticket.php", true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send();
+}
+function set_cards(response) {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var answer = JSON.parse(xmlhttp.responseText);
+
+            var flightlist = JSON.parse(response);
+            var cards = "";
+            var w = (window.innerWidth > 750) ? window.innerWidth : 750;
+            for(i = 0; i < flightlist.length; i++){
+                cards += "<div class='card' style='width:" + (w - 320) + "px;height:100px'><div style='padding:0px;color:#333;float:left'>" + flightlist[i].Airline + "<br/>";
+                cards += "From : " + flightlist[i].Source + "<br/>";
+                cards += "To : " + flightlist[i].Destination + "<br/>";
+        		cards += flightlist[i].Fare + " THB</div>";
+                if(answer.authen == 1)
+                    cards += '<div><button onclick="" type="button" class="addButton" style="float:right">ADD</button></div></div>';
+                else
+                    cards += '<div><button type="button" class="lockButton" style="float:right">LOGIN<br/>FIRST</button></div></div>';
+            }
+            document.getElementById("deck").innerHTML = cards;
+        }
+    }
+    xmlhttp.open("POST", "component/Account.php", true);
+    xmlhttp.send();
+}
